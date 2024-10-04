e33057ebf828186bf8080e74cedb439cd39e4b39
turtlegamer1114 <titus.taylor@malad.us>
2024-10-04 11:22:54 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Car mydino = new dino ("Dinosaur", "Trex");
+        
+        mydino.size(30);        // sets size to 30
+        mydino.speed(10);      // sets speed to 10
+        mydino.speed(25);      // changes speed to 25
+        mydino.strength(50);
+    }
+}
