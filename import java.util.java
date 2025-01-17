7c360b956ab393bdcf964848e9b698f062b8584e
turtlegamer1114 <titus.taylor@malad.us>
2025-01-16 16:54:59 -0800

    Add files via upload

---
+++ import java.util.java
@@ -0,0 +1,9 @@
+import java.util.Scanner
+
+public class Imputreading {
+    Scanner scanner = new Scanner(System.in);
+System.out.print("enter username:")
+    
+String name = scanner.nextline();
+System.out.printin("Hi"+ username);
+}
