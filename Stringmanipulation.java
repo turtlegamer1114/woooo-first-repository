171c04a053dd8717b3f19c46eeaa32dda706c9dd
turtlegamer1114 <titus.taylor@malad.us>
2025-03-06 13:23:56 -0800

    Add files via upload

--- Stringmanipulation.java
+++ Stringmanipulation.java
@@ -1,8 +1,10 @@
 // This is a String manipulation project created by Titus Taylor
+import java.util.Scanner;    
+
 public class Stringmanipulation {
     public static void main(String[] args) {
-        String s = "The quick brown fox jumped over the lazy dog ";
-        String[] words = s.split("\\W+"); // Split nno word characters
+        String s = "The quick brown fox jumped over the lazy small dog ";
+        String[] words = s.split("\\W+"); // Split no word characters
         
         int wordCount = 0; // create the word counter
         for (String word : words) {
@@ -11,5 +13,18 @@ public class Stringmanipulation {
         }
         
         System.out.println("number of words: " + wordCount);
+        Scanner scanner = new Scanner(System.in); // Create scanner
+       
+        System.out.print("Enter your string: "); // Create a string imput
+        String inputString = scanner.nextLine();
+        words = inputString.split("\\W+");
+         wordCount = 0; // create the word counter
+        for (String word : words) {
+            System.out.println(word);
+            wordCount++;
+        }
+        System.out.println("number of words: " + wordCount); // Print string wordcount
+    
+    
     }
 }
