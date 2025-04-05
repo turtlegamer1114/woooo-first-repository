1437b2074226fbc789052400da899b46cb741f42
turtlegamer1114 <titus.taylor@malad.us>
2025-04-04 18:02:17 -0700

    Delete Troubleshooting directory

--- Troubleshooting/Helloworld.java
+++
@@ -1,8 +0,0 @@
-// Titus Taylor Modified t his broken code. 
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-        System.out.println("this program has an error or two in it that should be simple to find"); //There was a missing Semi Colon here
-        System.out.println("I'm really good at making mistakes when programming"); // There was a colon here instead of semicolon
-    }
-}
